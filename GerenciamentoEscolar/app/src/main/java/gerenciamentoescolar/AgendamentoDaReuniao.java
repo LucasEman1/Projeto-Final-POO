64e39cfefa1c163939015b1f8023693903a09265
Lucas Emanuel <93333038+LucasEman1@users.noreply.github.com>
2024-09-18 16:41:59 -0300

    esboço de classes

---
+++ GerenciamentoEscolar/app/src/main/java/gerenciamentoescolar/AgendamentoDaReuniao.java
@@ -0,0 +1,51 @@
+package gerenciamentoescolar;
+
+import java.util.Date;
+
+public class AgendamentoDaReuniao {
+    private String assunto;
+    private Date dataDaReuniao;
+
+    public AgendamentoDaReuniao(String assunto, Date dataDaReuniao) {
+        this.assunto = assunto;
+        this.dataDaReuniao = dataDaReuniao;
+    }
+
+    // get e set
+    public String getAssunto() {
+        return assunto;
+    }
+
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+
+    public Date getDataDaReuniao() {
+        return dataDaReuniao;
+    }
+
+    public void setDataDaReuniao(Date dataDaReuniao) {
+        this.dataDaReuniao = dataDaReuniao;
+    }
+
+    // métodos
+    public void agendarReuniao() {
+
+    }
+
+    public void mostrarReuniao() {
+
+    }
+
+    public void removerReuniao() {
+
+    }
+
+    public void editarReuniao() {
+
+    }
+
+    public void visualizarReuniao() {
+
+    }
+}
---
+++ GerenciamentoEscolar/app/src/main/java/gerenciamentoescolar/Usuario.java
@@ -0,0 +1,121 @@
+package gerenciamentoescolar;
+
+import java.util.Date;
+
+public class Usuario {
+    private String nome;
+    private int senha;
+    private int cpf;
+    private String telefone;
+    private String cidade;
+    private String bairro;
+    private String rua;
+    private int numero;
+    private Date dataDeNascimento;
+
+    public Usuario() {
+        this.nome = nome;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.numero = numero;
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    // métodos
+    public void criarUsuario() {
+
+    }
+
+    public void editarUsuario() {
+
+    }
+
+    public void excluirUsuario() {
+
+    }
+
+    public void visualizarUsuario() {
+
+    }
+
+    public void listarUsuarios() {
+
+    }
+
+    // get e set
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+}
