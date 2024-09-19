a91e10a1bbbb9c386ad8868b07f2a6d84a3603b3
LisboaOliveira <124401762+LisboaOliveira@users.noreply.github.com>
2024-09-18 23:30:09 -0300

    commit

---
+++ GerenciamentoEscolar/app/src/main/java/gerenciamentoescolar/Matricula.java
@@ -0,0 +1,29 @@
+package gerenciamentoescolar;
+
+public class Matricula {
+    private String nomeDaInstituicao;
+    private int anoLetivo;
+    
+    public Matricula(String nomeDaInstituicao, int anoLetivo){
+        this.nomeDaInstituicao = nomeDaInstituicao;
+        this.anoLetivo = anoLetivo;
+    }
+
+    //Metodos 
+    public void fazerMatricula(){
+
+    }
+    public void editarMatricula(){
+
+    }
+    public void removerMatricula(){
+
+    }  
+    public void visualizarMatricula(){
+
+    } 
+    public void listatMatricula(){
+
+    } 
+    
+}
