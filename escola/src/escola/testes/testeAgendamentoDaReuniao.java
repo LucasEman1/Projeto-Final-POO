3f9154d6337887f5e8488e3d95b02c274a495d56
anaxgoras <99896088+anaxgoras@users.noreply.github.com>
2024-10-04 21:25:07 -0300

    commit

---
+++ escola/src/escola/testes/testeAgendamentoDaReuniao.java
@@ -0,0 +1,12 @@
+package escola.testes;
+
+import java.util.Scanner;
+
+import escola.AgendamentoDaReuniao;
+
+public class testeAgendamentoDaReuniao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        AgendamentoDaReuniao agendamento = new AgendamentoDaReuniao(null, null);
+    }
+}
