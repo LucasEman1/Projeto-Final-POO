5f41930dead6abd6ecf013059027482d4f5560b6
anaxgoras <99896088+anaxgoras@users.noreply.github.com>
2024-10-09 17:03:05 -0300

    comit

--- escola/src/escola/Instituicao.java
+++ escola/src/escola/Instituicao.java
@@ -1,4 +1,5 @@
-package escola;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Instituicao {
     private String nome;
@@ -25,24 +26,59 @@ public class Instituicao {
         this.endereco = endereco;
     }
 
-    public void criarInstituicao() {
-
+    public void exibirInformacoes() {
+        System.out.println("Nome da Instituição: " + nome);
+        System.out.println("Endereço: " + endereco);
     }
 
-    public void editarInstituicao() {
+    public static class GerenciadorInstituicoes {
+        private List<Instituicao> instituicoes = new ArrayList<>();
 
+        public void criarInstituicao(String nome, String endereco) {
+            Instituicao novaInstituicao = new Instituicao(nome, endereco);
+            instituicoes.add(novaInstituicao);
+            System.out.println("Instituição criada com sucesso!");
+        }
+
+        public void editarInstituicao(int index, String novoNome, String novoEndereco) {
+            if (index >= 0 && index < instituicoes.size()) {
+                Instituicao instituicao = instituicoes.get(index);
+                instituicao.setNome(novoNome);
+                instituicao.setEndereco(novoEndereco);
+                System.out.println("Instituição editada com sucesso!");
+            } else {
+                System.out.println("indice inválido!");
+            }
+        }
+        public void excluirInstituicao(int index) {
+            if (index >= 0 && index < instituicoes.size()) {
+                instituicoes.remove(index);
+                System.out.println("Instituição excluída com sucesso!");
+            } else {
+                System.out.println("indice inválido!");
+            }
+        }
+        public void listarInstituicoes() {
+            for (int i = 0; i < instituicoes.size(); i++) {
+                System.out.println("Instituiçao" + (i + 1) + ":");
+                instituicoes.get(i).exibirInformacoes();
+                System.out.println();
+            }
+        }
     }
 
-    public void removerInstituicao() {
+    public static void main(String[] args) {
+        GerenciadorInstituicoes gerenciador = new GerenciadorInstituicoes();
 
+        gerenciador.criarInstituicao("Escola ABC", "Rua das Flores, 123");
+        gerenciador.criarInstituicao("Escola XYZ", "Avenida Central, 456");
+
+        gerenciador.listarInstituicoes();
+
+        gerenciador.editarInstituicao(0, "Escola ABCD", "Rua das Rosas, 789");
+
+        gerenciador.excluirInstituicao(1);
+
+        gerenciador.listarInstituicoes();
     }
-
-    public void listarInstituicao() {
-
-    }
-
-    public void visualizarInstituicao() {
-
-    }
-
 }
