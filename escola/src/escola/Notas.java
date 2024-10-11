ee1e713d79fbbba43765a869970bd77be4e097e4
LucasEman1 <93333038+LucasEman1@users.noreply.github.com>
2024-10-11 13:25:15 -0300

    atualização notas

--- escola/src/escola/Notas.java
+++ escola/src/escola/Notas.java
@@ -1,5 +1,6 @@
 package escola;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Notas {
@@ -9,74 +10,152 @@ public class Notas {
     private float media;
     private String situacao;
 
-    public Notas(float nota1, float nota2, float nota3, float media, String situacao) {
+    public Notas(float nota1, float nota2, float nota3) {
         this.nota1 = nota1;
         this.nota2 = nota2;
         this.nota3 = nota3;
-        this.media = media;
-        this.situacao = situacao;
+        calcularMedia();
+        calculaSituacao();
+    }
+
+    private float calcularMedia() {
+        return media = (nota1 + nota2 + nota3) / 3;
+    }
+
+    private String calculaSituacao() {
+        return situacao = media > 7.0 ? "Você está aprovado" : "Você está reprovado";
     }
 
     public void criarNotas(Scanner sc) {
-        try {
-            System.out.println("Digite a nota 1: ");
-            nota1 = sc.nextFloat();
-            System.out.println("Digite a nota 2: ");
-            nota2 = sc.nextFloat();
-            System.out.println("Digite a nota 3: ");
-            nota3 = sc.nextFloat();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        System.out.print("Digite a nota 1: ");
+        nota1 = sc.nextFloat();
+        System.out.println("");
 
-        media = (nota1 + nota2 + nota3) / 3;
+        System.out.print("Digite a nota 2: ");
+        nota2 = sc.nextFloat();
+        System.out.println("");
 
-        if (media >= 7.0) {
-            situacao = "Aprovado";
-        } else {
-            situacao = "Em recuperação";
-        }
+        System.out.print("Digite a nota 3: ");
+        nota3 = sc.nextFloat();
+        System.out.println("");
 
+        calcularMedia();
+        calculaSituacao();
         System.out.println(toString());
     }
 
+    /*
+     * public void salvarNotas(String caminhoArquivo) {
+     * try (FileWriter salva = new FileWriter(caminhoArquivo, true)) {
+     * BufferedWriter escreve = new BufferedWriter(salva);
+     * salva.write(nota1 + "\n" + nota2 + "\n" + nota3 + "\n" + media + "\n" +
+     * situacao + "\n");
+     * System.out.println("Notas salvas com sucesso!");
+     * } catch (IOException e) {
+     * System.out.println("Erro ao salvar notas: " + e.getMessage());
+     * }
+     * }
+     * // Método para carregar as notas do arquivo
+     * public void carregarNotas(String caminhoArquivo) {
+     * try (BufferedReader reader = new BufferedReader(new
+     * FileReader(caminhoArquivo))) {
+     * nota1 = Float.parseFloat(reader.readLine());
+     * nota2 = Float.parseFloat(reader.readLine());
+     * nota3 = Float.parseFloat(reader.readLine());
+     * media = Float.parseFloat(reader.readLine());
+     * situacao = reader.readLine();
+     * System.out.println("Notas carregadas com sucesso!");
+     * } catch (IOException e) {
+     * System.out.println("Erro ao carregar notas: " + e.getMessage());
+     * }
+     * }
+     */
+
     public void editarNotas(Scanner sc) {
-        System.out.println("Digite a nota que deseja editar: ");
-        int decide = sc.nextInt();
-        switch (decide) {
+        System.out.print("Deseja editar qual nota? 1|2|3 ");
+        int qual = sc.nextInt();
+
+        switch (qual) {
             case 1:
-                System.out.println("Nova nota primeira unidade: ");
-                nota1 = sc.nextFloat();
+                System.out.print("Nova nota: ");
+                setNota1(sc.nextFloat());
+                calcularMedia();
+
+                System.out.println(toString());
                 break;
             case 2:
-                System.out.println("Nova nota segunda unidade: ");
-                nota2 = sc.nextFloat();
+                System.out.print("Nova nota: ");
+                setNota2(sc.nextFloat());
+                calcularMedia();
+
+                System.out.println(toString());
                 break;
             case 3:
-                System.out.println("Nova nota terceira unidade: ");
-                nota3 = sc.nextFloat();
+                System.out.print("Nova nota: ");
+                setNota3(sc.nextFloat());
+                calcularMedia();
+
+                System.out.println(toString());
                 break;
             default:
+                System.out.println("Opção não existe");
                 break;
         }
     }
 
-    public void excluirNotas() {
-
+    public void excluirNotas(Scanner sc) {
+        System.out.print("Deseja excluir qual nota? 1|2|3 ");
+        int qual = sc.nextInt();
+        switch (qual) {
+            case 1:
+                try {
+                    setNota1(0);
+                    System.out.println("Nota excluida!");
+                    calcularMedia();
+                } catch (Exception e) {
+                    System.out.println("Erro" + e.getMessage());
+                }
+                break;
+            case 2:
+                try {
+                    setNota2(0);
+                    System.out.println("Nota excluida!");
+                    calcularMedia();
+                } catch (Exception e) {
+                    System.out.println("Erro" + e.getMessage());
+                }
+                break;
+            case 3:
+                try {
+                    setNota3(0);
+                    System.out.println("Nota excluida!");
+                    calcularMedia();;
+                } catch (Exception e) {
+                    System.out.println("Erro" + e.getMessage());
+                }
+                break;
+            default:
+                System.out.println("Opcão não existe");
+                break;
+        }
     }
 
     public void visualizarNotas() {
-
-    }
-
-    public void listarNotas() {
-
+        System.out.println(this);
     }
 
     @Override
     public String toString() {
         return "Notas [nota1=" + nota1 + ", nota2=" + nota2 + ", nota3=" + nota3 + ", media=" + media + ", situacao="
                 + situacao + "]";
+    }
+
+    public float getMedia() {
+        return media;
+    }
+
+    public void setMedia(float media) {
+        this.media = media;
     }
 
     public float getNota1() {
@@ -101,14 +180,6 @@ public class Notas {
 
     public void setNota3(float nota3) {
         this.nota3 = nota3;
-    }
-
-    public float getMedia() {
-        return media;
-    }
-
-    public void setMedia(float media) {
-        this.media = media;
     }
 
     public String getSituacao() {
--- escola/src/escola/testes/testeNotas.java
+++ escola/src/escola/testes/testeNotas.java
@@ -6,9 +6,39 @@ import java.util.Scanner;
 
 public class testeNotas {
     public static void main(String[] args) {
-        Notas notas = new Notas(0, 0, 0, 0, null);
+        //String caminhoArquivo = "src\\escola\\arquivos\\notas.txt";
+        Notas notas = new Notas(0, 0, 0);
         Scanner sc = new Scanner(System.in);
-
-        notas.criarNotas(sc);
+        int caso;
+        do {
+            System.out.println(" ");
+            System.out.println("Criar Nota: 1 ");
+            System.out.println("Editar Nota: 2 ");
+            System.out.println("Excluir Nota: 3 ");
+            System.out.println("Visualizar Notas: 4 ");
+            System.out.print("opcao: ");
+            caso = sc.nextInt();
+            //Notas notasCarregadas = new Notas(0, 0, 0);
+            //notasCarregadas.carregarNotas(caminhoArquivo); 
+            switch (caso) {
+                case 1:
+                    notas.criarNotas(sc);
+                    //notas.salvarNotas(caminhoArquivo);
+                    break;
+                case 2:
+                    notas.editarNotas(sc);
+                    break;
+                case 3:
+                    notas.excluirNotas(sc);
+                    break;
+                case 4:
+                    notas.visualizarNotas();
+                    //notasCarregadas.visualizarNotas();
+                    break;
+                default:
+                    break;
+            }
+        } while (caso != 0);
+        sc.close();
     }
 }
