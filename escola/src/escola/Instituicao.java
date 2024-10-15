c521c1751b3ce28fc066ab844506858f5dc8a4b3
anaxgoras <99896088+anaxgoras@users.noreply.github.com>
2024-10-15 15:20:15 -0300

    Update Instituicao.java
    commit

--- escola/src/escola/Instituicao.java
+++ escola/src/escola/Instituicao.java
@@ -1,5 +1,6 @@
-import java.util.ArrayList;
+C:\Users\Usuário\Documents\Projeto-Final-POO\escola\src\escola\Projeto-Final-POO\escola\src\escola\Instituicao.javaimport java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Instituicao {
     private String nome;
@@ -47,20 +48,22 @@ public class Instituicao {
                 instituicao.setEndereco(novoEndereco);
                 System.out.println("Instituição editada com sucesso!");
             } else {
-                System.out.println("indice inválido!");
+                System.out.println("Índice inválido!");
             }
         }
+
         public void excluirInstituicao(int index) {
             if (index >= 0 && index < instituicoes.size()) {
                 instituicoes.remove(index);
                 System.out.println("Instituição excluída com sucesso!");
             } else {
-                System.out.println("indice inválido!");
+                System.out.println("Índice inválido!");
             }
         }
+
         public void listarInstituicoes() {
             for (int i = 0; i < instituicoes.size(); i++) {
-                System.out.println("Instituiçao" + (i + 1) + ":");
+                System.out.println("Instituição " + (i + 1) + ":");
                 instituicoes.get(i).exibirInformacoes();
                 System.out.println();
             }
@@ -69,16 +72,52 @@ public class Instituicao {
 
     public static void main(String[] args) {
         GerenciadorInstituicoes gerenciador = new GerenciadorInstituicoes();
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
 
-        gerenciador.criarInstituicao("Escola ABC", "Rua das Flores, 123");
-        gerenciador.criarInstituicao("Escola XYZ", "Avenida Central, 456");
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Criar Instituição");
+            System.out.println("2. Editar Instituição");
+            System.out.println("3. Excluir Instituição");
+            System.out.println("4. Listar Instituições");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine(); 
 
-        gerenciador.listarInstituicoes();
-
-        gerenciador.editarInstituicao(0, "Escola ABCD", "Rua das Rosas, 789");
-
-        gerenciador.excluirInstituicao(1);
-
-        gerenciador.listarInstituicoes();
+            switch (opcao) {
+                case 1:
+                    System.out.print("Nome da Instituição: ");
+                    String nome = scanner.nextLine();
+                    System.out.print("Endereço da Instituição: ");
+                    String endereco = scanner.nextLine();
+                    gerenciador.criarInstituicao(nome, endereco);
+                    break;
+                case 2:
+                    System.out.print("Índice da Instituição a ser editada: ");
+                    int indiceEditar = scanner.nextInt();
+                    scanner.nextLine(); 
+                    System.out.print("Novo nome da Instituição: ");
+                    String novoNome = scanner.nextLine();
+                    System.out.print("Novo endereço da Instituição: ");
+                    String novoEndereco = scanner.nextLine();
+                    gerenciador.editarInstituicao(indiceEditar, novoNome, novoEndereco);
+                    break;
+                case 3:
+                    System.out.print("Índice da Instituição a ser excluída: ");
+                    int indiceExcluir = scanner.nextInt();
+                    gerenciador.excluirInstituicao(indiceExcluir);
+                    break;
+                case 4:
+                    gerenciador.listarInstituicoes();
+                    break;
+                case 0:
+                    System.out.println("Saindo do programa.");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+        } while (opcao != 0);
     }
 }
